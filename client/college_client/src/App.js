645dd6c7c6caf37ac28f270717281568c0125bb5
apatwa17 <ahpandroid5@gmail.com>
2019-04-23 20:11:23 -0500

    Messing with cards

--- client/college_client/src/App.js
+++ client/college_client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import { Card } from "react-bootstrap";
 import "./App.css";
 
@@ -7,21 +7,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <Card>
+        <Card style={{ width: "14rem" }}>
+          <Card.Body>
+            <Card.Title>Some title</Card.Title>
+            <Card.Text>Some text</Card.Text>
+          </Card.Body>
+        </Card>
+        <Card style={{ width: "14rem" }}>
           <Card.Body>
             <Card.Title>Some title</Card.Title>
             <Card.Text>Some text</Card.Text>
